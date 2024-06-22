3a041b26bb49a3a103e7ae9b55b0df18b605fd12
manishrathore500343@gmail.com <manishrathore500343@gmail.com>
2024-06-22 14:46:30 +0530

    new format added

--- preprocessor.py
+++ preprocessor.py
@@ -1,23 +1,34 @@
 import re
 import pandas as pd
 
+
 def preprocess(data):
-    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    # Update the pattern to match both 12-hour and 24-hour time formats
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s(?:AM|PM|am|pm|-)\s-\s|\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
 
     messages = re.split(pattern, data)[1:]
     dates = re.findall(pattern, data)
 
-    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
-    # convert message_date type
-    df['message_date'] = pd.to_datetime(df['message_date'], format='%d/%m/%Y, %H:%M - ')
+    # Remove any trailing ' - ' from the date strings
+    dates = [date.strip(' -') for date in dates]
 
-    df.rename(columns={'message_date': 'date'}, inplace=True)
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+
+    # Convert message_date type
+    def parse_date(date_str):
+        for fmt in ('%d/%m/%Y, %I:%M %p', '%d/%m/%Y, %H:%M'):
+            try:
+                return pd.to_datetime(date_str, format=fmt)
+            except ValueError:
+                pass
+        raise ValueError(f"no valid date format found for {date_str}")
+
+    df['date'] = df['message_date'].apply(parse_date)
 
     users = []
     messages = []
     for message in df['user_message']:
         entry = re.split('([\w\W]+?):\s', message)
-        print(entry)
         if entry[1:]:  # user name
             users.append(entry[1])
             messages.append(" ".join(entry[2:]))
@@ -27,7 +38,7 @@ def preprocess(data):
 
     df['user'] = users
     df['message'] = messages
-    df.drop(columns=['user_message'], inplace=True)
+    df.drop(columns=['user_message', 'message_date'], inplace=True)
 
     df['only_date'] = df['date'].dt.date
     df['year'] = df['date'].dt.year
@@ -41,9 +52,9 @@ def preprocess(data):
     period = []
     for hour in df[['day_name', 'hour']]['hour']:
         if hour == 23:
-            period.append(str(hour) + "-" + str('00'))
+            period.append(str(hour) + "-00")
         elif hour == 0:
-            period.append(str('00') + "-" + str(hour + 1))
+            period.append("00-01")
         else:
             period.append(str(hour) + "-" + str(hour + 1))
 
